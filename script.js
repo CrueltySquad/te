7cb5a662f9a956120c763e762f5151126f21ac2e
CrueltySquad <152569790+CrueltySquad@users.noreply.github.com>
2024-02-24 15:48:56 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-document.getElementById('main-tab').addEventListener('click', () => {
-  document.getElementById('main-content').classList.add('active');
-  document.getElementById('updates-content').classList.remove('active');
-  document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
+document.('main-tab').addEventListener('click () => {
+  document.getElementById('-content').classListadd('active');
+  document.getElementByIdupdates-content')..remove('active');
+  document.querySelectorAll('.tab').forEach(tab => tab.classList.remove(''));
   document.getElementById('main-tab').classList.add('active');
 });
 
 document.getElementById('updates-tab').addEventListener('click', () => {
   document.getElementById('main-content').classList.remove('active');
-  document.getElementById
+  document.getElementById('updates-content').classList.add('active');
+  document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
+  document.getElementById('updates-tab').classList.add('active');
+});
